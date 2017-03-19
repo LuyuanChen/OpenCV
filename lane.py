378fc1175122e45940860c428e99d37b50e545d0
Luyuan Chen <nonofyour@busi.ness>
2017-03-19 18:27:20 -0400

    - removed duplicated lines, changed plotting property

--- lane.py
+++ lane.py
@@ -70,21 +70,29 @@ def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
     lines = cv2.HoughLinesP(img, rho, theta, threshold,
                             np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
     line_img = np.zeros(img.shape, dtype=np.uint8)
+
+    plotted_slopes = []  # an int (rounded) array for elliminating duplicates
+    slope = 0
+
     for i in xrange(lines.shape[0]):
         for x1,y1,x2,y2 in lines[i]:
-            if abs(y2-y1) > 50:
+            slope = np.round(10*float(y2-y1)/(x2-x1))
+            print "line slope:" + str(slope)
+            if abs(y2-y1) > 50 and slope not in plotted_slopes:
                 cv2.line(line_img, (x1, y1), (x2, y2), (255, 255, 255), 2)
-
+                plotted_slopes.append(slope)
     return line_img
 
-def weighted_img(edges, line_img, alpha=0.9, beta=1., upsilon=0.):
-    color_edges = np.dstack((edges, edges, edges))
+def weighted_img(edges, line_img, alpha=1., beta=0.8, upsilon=0.5):
+    color_edges = np.dstack((edges, edges, np.copy(edges) * 0))
     return cv2.addWeighted(color_edges, alpha, line_img, beta, upsilon)
 
 def get_files(dir_path):
     return [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
 
 def process_img(name, img):
+    # ref: https://github.com/paramaggarwal/CarND-LaneLines-P1/blob/master/P1.ipynb
+
     dir_name = "output_images"
     line_image = np.copy(img)*0
 	
@@ -97,7 +105,7 @@ def process_img(name, img):
     # masked_edges = region_of_interest(edges, vertices)
     
     lines = hough_lines(edges, 1, 5*np.pi/180, 100, 30, 100)
-    final_img = weighted_img(lines, img)
+    final_img = weighted_img(lines, cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
     cv2.imshow('', final_img)
     cv2.waitKey()
     # save_plot(dir_name, "final_"+name, final_img)
