e24a7121abd9eef3605a765c7fe41684afad6425
Ming Yue <mingyue312@gmail.com>
2017-03-28 18:04:10 -0400

    Added profiler and changed rec_disp to a function

---
+++ cprofile.py
@@ -0,0 +1,7 @@
+import cProfile
+import rec_disp as rd
+
+cProfile.run("rd.rec_disp('000116.jpg')")
+
+
+# 9.5 cm
--- rec_disp.py
+++ rec_disp.py
@@ -1,53 +1,59 @@
 # import the necessary packages
-import argparse
-import cv2
+# import argparse
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 
 # construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-                help="path to the input image")
-ap.add_argument("-c", "--cascade",
-                default="./cascade_xml/cars.xml",
-                help="path to car detector haar cascade")
-args = vars(ap.parse_args())
 
-# load the input image and convert it to grayscale
-image_name = args["image"]
 
-IMAGE_DIR = './images/'
-LEFT_IMAGE_DIR = IMAGE_DIR + 'left/'
-RIGHT_IMAGE_DIR = IMAGE_DIR + 'right/'
-imgL = cv2.imread(LEFT_IMAGE_DIR + image_name, 0)
-imgR = cv2.imread(RIGHT_IMAGE_DIR + image_name, 0)
+def rec_disp(image_name):
+    # ap = argparse.ArgumentParser()
+    # ap.add_argument("-i", "--image", required=True,
+    #                 help="path to the input image")
+    # ap.add_argument("-c", "--cascade",
+    #                 default="./cascade_xml/cars.xml",
+    #                 help="path to car detector haar cascade")
+    # args = vars(ap.parse_args())
 
-# load the car detector Haar cascade, then detect cars in the input image
-detector = cv2.CascadeClassifier(args["cascade"])
-rects = detector.detectMultiScale(imgL, scaleFactor=1.04, #1.1
-                                  minNeighbors=5, minSize=(45, 45)) #minNeighbors=10, minSize=(45, 45))
-# get disparity
-stereo = cv2.StereoBM_create(96, 15)
-disparity = stereo.compute(imgL, imgR)
+    # load the input image and convert it to grayscale
+    # image_name = args["image"]
 
-# loop over the cars and draw a rectangle surrounding each
-for (i, (x, y, w, h)) in enumerate(rects):
-    cv2.rectangle(imgL, (x, y), (x + w, y + h), (0, 0, 255), 2)
-    # get tile from disparity
-    tile = disparity[y:y + h, x:x + w]
-    coor = np.where(tile==tile.max())
-    coor = [coor[0][0],coor[1][0]]
-    avg = tile[coor[0]-w/8:coor[0]+w/8,coor[1]-h/8:coor[1]+h/8].mean()
-    print('Car #' + str(i) +' dist' + str(avg))
-    cv2.putText(imgL, "Car #{}".format(i), (x, y - 10),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
+    IMAGE_DIR = './images/'
+    LEFT_IMAGE_DIR = IMAGE_DIR + 'left/'
+    RIGHT_IMAGE_DIR = IMAGE_DIR + 'right/'
+    imgL = cv2.imread(LEFT_IMAGE_DIR + image_name, 0)
+    imgR = cv2.imread(RIGHT_IMAGE_DIR + image_name, 0)
 
-# show the detected cars
-cv2.imshow("Cars", disparity)
-cv2.waitKey(3)
+    # load the car detector Haar cascade, then detect cars in the input image
+    detector = cv2.CascadeClassifier("./cascade_xml/cars.xml") #(args["cascade"])
+    rects = detector.detectMultiScale(imgL, scaleFactor=1.04, #1.1
+                                      minNeighbors=5, minSize=(45, 45)) #minNeighbors=10, minSize=(45, 45))
+    # get disparity
+    stereo = cv2.StereoBM_create(96, 15)
+    disparity = stereo.compute(imgL, imgR)
 
-# show the disparity
-plt.imshow(disparity, 'gray')
-plt.show()
+    # loop over the cars and draw a rectangle surrounding each
+    for (i, (x, y, w, h)) in enumerate(rects):
+        # cv2.rectangle(imgL, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        # get tile from disparity
+        tile = disparity[y:y + h, x:x + w]
+        coor = np.where(tile==tile.max())
+        coor = [coor[0][0],coor[1][0]]
+        avg = tile[coor[0]-w/8:coor[0]+w/8,coor[1]-h/8:coor[1]+h/8].mean()
+        print('Car #' + str(i) +' dist' + str(avg))
+        # cv2.putText(imgL, "Car #{}".format(i), (x, y - 10),
+            # cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
+
+    # show the detected cars
+    # cv2.imshow("Cars", disparity)
+    # cv2.waitKey(3)
+
+    # show the disparity
+    # plt.imshow(disparity, 'gray')
+    # plt.show()
+
+
+for i in range(100):
+    rec_disp('000116.jpg')
 
